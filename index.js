fcf1e7fd7773f9720838abb2490ee0bc13da8a35
Clish254 <clishilla@gmail.com>
2020-12-23 18:48:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const app = express();
+
+app.use(cors());
+app.use(express.static("build"));
+
+app.get("/", (req, res) => {
+  res.send("afreeca invest api");
+});
+
+app.get("/api/last_quote/stocks/:symbol", async (req, res) => {
+  const { symbol } = req.params;
+  const url = "https://data.alpaca.markets/v1/last_quote/stocks/";
+  const headers = {
+    "APCA-API-KEY-ID": process.env.alpaca_key_id,
+    "APCA-API-SECRET-KEY": process.env.alpaca_secret_key,
+  };
+  try {
+    response = await axios({
+      method: "get",
+      url: `${url}/${symbol}`,
+      headers,
+    });
+    return res.status(200).json({
+      success: "true",
+      data: response.data,
+    });
+  } catch (error) {
+    if (error.response.status === 404) {
+      return res.status(404).json({
+        success: "false",
+        data: {
+          error: error.response.message,
+        },
+      });
+    }
+    return res.status(500).json({
+      success: "false",
+      data: {
+        error: error.response.message,
+      },
+    });
+  }
+});
+
+app.get("/api/marketCap/:symbol", async (req, res) => {
+  const { symbol } = req.params;
+  const url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=";
+  try {
+    response = await axios({
+      method: "get",
+      url: `${url}${symbol}&f=j1`,
+    });
+    return res.status(200).json({
+      success: "true",
+      data: response.data,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: "false",
+      data: {
+        error: error.response.message,
+      },
+    });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
